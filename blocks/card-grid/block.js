d228e6ec89e226431d215cf220957b1df560ab88
aaronhans <aaronhans@gmail.com>
2021-04-28 08:25:10 -0700

    latest card grid that uses cards internally

--- blocks/card-grid/block.js
+++ blocks/card-grid/block.js
@@ -2,86 +2,31 @@
  * CAGov Card Grid
  *
  */
- ( function( blocks, editor, i18n, element, components, _ ) {
-	var __ = i18n.__;
+ ( function( blocks, element, blockEditor ) {
 	var el = element.createElement;
-	var RichText = editor.RichText;
-
+	var InnerBlocks = blockEditor.InnerBlocks;
+	const ALLOWED_BLOCKS = [ 'cagov/card' ];
 	blocks.registerBlockType( 'cagov/card-grid', {
-		title: __( 'CAGov: Card Grid', 'cagov-design-system' ),
-		icon: 'universal-access-alt',
+		title: 'CAGov card grid',
 		category: 'layout',
-		attributes: {
-			title: {
-				type: 'array',
-				source: 'children',
-				selector: 'h3',
-			},
-			body: {
-				type: 'array',
-				source: 'children',
-				selector: 'p',
-			}
-		},
-		example: {
-			attributes: {
-				title: __( 'Card Grid title', 'cagov-design-system' ),
-				body: __( 'Card Grid body', 'cagov-design-system' )
-			}
-		},
 		edit: function( props ) {
-			var attributes = props.attributes;
-
 			return el(
 				'div',
-				{ className: 'cagov-card-grid cagov-stack' },
-				el( RichText, {
-					tagName: 'h3',
-					inline: true,
-					placeholder: __(
-						'Write card title…',
-						'cagov-design-system'
-					),
-					value: attributes.title,
-					onChange: function( value ) {
-						props.setAttributes( { title: value } );
-					},
-				} ),
-				el( RichText, {
-					tagName: 'p',
-					inline: true,
-					placeholder: __(
-						'Write card body',
-						'cagov-design-system'
-					),
-					value: attributes.body,
-					onChange: function( value ) {
-						props.setAttributes( { body: value } );
-					},
-				} )
+				{ className: 'cagov-grid' },
+				el( InnerBlocks,
+					{
+						orientation:"horizontal",
+						allowedBlocks: ALLOWED_BLOCKS
+					}
+				)
 			);
 		},
-		save: function(props) {
-			var attributes = props.attributes;
+		save: function( props ) {
 			return el(
 				'div',
-				{ className: 'cagov-card cagov-stack' },
-				el( RichText.Content, {
-					tagName: 'h3',
-					value: attributes.title,
-				} ),
-				el( RichText.Content, {
-					tagName: 'p',
-					value: attributes.body,
-				} )
+				{ className: 'cagov-grid' },
+				el( InnerBlocks.Content )
 			);
 		},
 	} );
-} )(
-	window.wp.blocks,
-	window.wp.editor,
-	window.wp.i18n,
-	window.wp.element,
-	window.wp.components,
-	window._
-);
+} )( window.wp.blocks, window.wp.element, window.wp.blockEditor );
