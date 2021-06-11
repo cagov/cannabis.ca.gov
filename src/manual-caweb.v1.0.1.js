a04665dfe47bb4d0c29b7a971da5927db0811b18
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-06-11 10:37:19 -0700

    Updated  manual JS file for CAWeb options

--- src/manual-caweb.v1.0.1.js
+++ src/manual-caweb.v1.0.1.js
@@ -1,4 +1,4 @@
-// This to make sure featured search is visible and displayed correctly
+// This is to make sure featured search is visible and displayed correctly
 $( document ).ready(function() {
     $(".navigation-search").addClass("full-width-nav container");
     $("#head-search").addClass("featured-search");
