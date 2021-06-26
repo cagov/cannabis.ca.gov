c6aca0b49f68c86c2f35fb2265cd0d723b225e98
Aaron Hans <aaronhans@gmail.com>
2021-06-25 17:19:54 -0700

    retrieve menus from wp

--- src/templates/_data/announcements.js
+++ src/templates/_data/announcements.js
@@ -1,6 +1,4 @@
 // fetch last 3 announcements for homepage
-// https://staginginye.prod3.sites.ca.gov/wp-json/wp/v2/posts?categories=14,16&per_page=10&order=desc&page=1
-
 const fetch = require('node-fetch')
 
 module.exports = function() {
--- src/templates/_data/menus.js
+++ src/templates/_data/menus.js
@@ -1,1 +1,27 @@
-// fetch top and bottom menus
+// fetch header, footer and content menus
+
+const fetch = require('node-fetch')
+let urlBase = 'https://staginginye.prod3.sites.ca.gov/wp-json/menus/v1/menus';
+module.exports = function () {
+  return new Promise((resolve, reject) => {
+    fetch(urlBase)
+      .then(res => res.json())
+      .then(json => {
+        let idsToRetrieve = [];
+        let desiredSlugs = ['content-menu', 'footer-menu', 'header-menu']
+        json.forEach(item => {
+          if (desiredSlugs.includes(item.slug)) {
+            idsToRetrieve.push(item.term_id)
+          }
+        })
+        Promise.all(
+          idsToRetrieve.map(id =>
+            fetch(`${urlBase}/${id}`)
+              .then(e => e.json())
+          )
+        ).then(data => {
+          resolve(data.flat());
+        });
+      });
+  });
+};
