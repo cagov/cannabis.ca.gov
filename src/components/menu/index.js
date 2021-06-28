36da785f6ad7f0293522cb8ad7929b90994d76a9
Aaron Hans <aaronhans@gmail.com>
2021-06-27 18:51:17 -0700

    menus working little better

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -2,7 +2,6 @@ console.log('wtf');
 
 class CAGOVOverlayNav extends window.HTMLElement {
   connectedCallback () {
-    console.log('eh')
     this.menuContentFile = this.dataset.json;
     this.querySelector('.open-menu').addEventListener('click', this.toggleMainMenu.bind(this));
     console.log('expansion listeners call')
@@ -23,15 +22,6 @@ class CAGOVOverlayNav extends window.HTMLElement {
     this.querySelector('.menu-trigger').classList.add('is-fixed');
     var menLabel = this.querySelector('.menu-trigger-label');
     menLabel.innerHTML = menLabel.getAttribute('data-closelabel');
-    this.querySelector('#main-menu').setAttribute('aria-hidden', 'false');
-    document.addEventListener('keydown', this.escapeMainMenu.bind(this));
-    setTimeout(
-      function () {
-        this.classList.add('reveal-items');
-        this.querySelector('#search-site').focus();
-      }.bind(this),
-      300
-    );
   }
 
   closeMainMenu () {
@@ -41,19 +31,10 @@ class CAGOVOverlayNav extends window.HTMLElement {
     this.querySelector('.menu-trigger').classList.remove('is-fixed');
     var menLabel = this.querySelector('.menu-trigger-label');
     menLabel.innerHTML =  menLabel.getAttribute('data-openlabel');
-    // what should we apply aria-expanded to?
-    this.querySelector('#main-menu').setAttribute('aria-hidden', 'true');
-    document.removeEventListener('keydown', this.escapeMainMenu.bind(this));
-  }
-
-  escapeMainMenu (event) {
-    // Close the menu if user presses escape key.
-    if (event.keyCode === 27) { this.closeMainMenu(); }
   }
 
   expansionListeners () {
     const allMenus = this.querySelectorAll('.js-expandable-mobile');
-    console.log(allMenus)
     allMenus.forEach(menu => {
       const nearestMenu = menu.closest('.expanded-menu-section');
       if (nearestMenu) {
@@ -65,7 +46,6 @@ class CAGOVOverlayNav extends window.HTMLElement {
       }
       menu.addEventListener('click', function (event) {
         event.preventDefault();
-        console.log(this)
         this.closest('.expanded-menu-section').classList.toggle('expanded');
         this.closest('.expanded-menu-col').setAttribute('aria-expanded', 'true');
         const closestDropDown = this.closest('.expanded-menu-section').querySelector('.expanded-menu-dropdown');
