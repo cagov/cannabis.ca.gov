500a6b82274c9794d670ffef0934cbf2b88e634a
chachasikes <149436+chachasikes@users.noreply.github.com>
2021-08-04 11:02:29 -0700

    Update manual CSS and JS

--- src/manual-caweb.v1.0.1.js
+++ src/manual-caweb.v1.0.1.js
@@ -56,3 +56,35 @@ let newScript2 = document.createElement("script");
 newScript2.src = "https://ethn.io/17561.js";
 newScript2.async = "true";
 document.querySelector('head').appendChild(newScript2);
+
+// Adding pdf span to the links with class pdf
+var pdf = '<span class="pdf-link-icon no-underline-icon" aria-hidden="true">.PDF</span><span class="sr-only"> (this is a pdf file)</span>';
+
+// selector is looking for links with pdf extension in the href
+var pdfLink = document.querySelectorAll("a[href*='.pdf']");
+for (var i=0; i < pdfLink.length; i++) {
+  pdfLink[i].innerHTML+=pdf; // += concatenates to pdf links
+}
+
+
+// Adding external link icon to the external links
+var ext = '<span class="ca-gov-icon-external-link link-icon" aria-hidden="true"></span>';
+// Check if link is external function
+function link_is_external(link_element) {
+  return (link_element.host !== window.location.host);
+}
+
+/*
+// Looping thru all link inside of the internal pages main content body
+var externalLink = document.querySelectorAll(".container .row .col-lg-10 a");
+for (var i = 0; i < externalLink.length; i++) {
+  var anchorLink = externalLink[i].href.indexOf("#") > -1;
+  var localHost = externalLink[i].href.indexOf("localhost") > -1;
+  var localUrl = externalLink[i].href.indexOf("cannabis.ca.gov") > -1;
+  var localEmail = externalLink[i].href.indexOf("@") > -1;
+  if (link_is_external(externalLink[i]) && !localUrl && !localHost && !anchorLink && !localEmail) {
+    externalLink[i].innerHTML += ext; // += concatenates to external links
+
+  }
+}*/
+
