dc9e84a753ce057c7cfc63ec29aca7f3a1e5f852
Aaron Hans <aaronhans@gmail.com>
2021-08-06 11:27:54 -0700

    remove step list and unused component

--- src/components/alert/index.js
+++
@@ -1,6 +0,0 @@
-const btnclose = document.querySelector('button.btn-close');
-const cagovalert = document.querySelector('.cagov-alert');
-
-btnclose.addEventListener('click', event => {
-    cagovalert.remove();
-});
