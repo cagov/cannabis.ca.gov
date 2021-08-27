20d913aca98f221bf532611159b16f6a8602a765
Aaron Hans <aaronhans@gmail.com>
2021-08-27 09:24:55 -0700

    testing against local server now

--- tests/index.spec.mjs
+++ tests/index.spec.mjs
@@ -1,7 +1,17 @@
 import { test, expect } from '@playwright/test';
+import { spawn } from 'child_process';
+let devserver;
+
+test.beforeAll(async () => {
+  devserver = spawn('npm', ['run', 'test:server']);
+})
 
 test('basic test', async ({ page }) => {
-  await page.goto('https://headless.cannabis.ca.gov/');
+  await page.goto('http://localhost:8000/');
   const title = page.locator('h2  >> nth=0');
   await expect(title).toContainText('changes');
+});
+
+test.afterAll(async () => {
+  devserver.kill('SIGINT');
 });
