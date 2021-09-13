af388b260d3cc7459e984529c3d773ad91526645
kkoryaka <31669748+kkoryaka@users.noreply.github.com>
2021-09-13 15:10:52 -0700

    added pdf icon component references #74

--- src/js/index-headless.js
+++ src/js/index-headless.js
@@ -6,6 +6,7 @@ import '@cagov/ds-plus';
 import '@cagov/ds-google-translate';
 import '@cagov/ds-dropdown-menu';
 import '@cagov/ds-content-navigation';
+import '@cagov/ds-pdf-icon/src/index.js';
 
 import '../components/post-list-headless/index.js';
 import '../components/page-alert/index.js';
