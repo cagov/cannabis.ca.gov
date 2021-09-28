403b5303bb7e069ce4f5db8e11ddbc6eda49ce0f
chachasikes <149436+chachasikes@users.noreply.github.com>
2021-09-28 11:14:57 -0700

    sync page includes

---
+++ pages/_includes/layouts/templates/press-list.js
@@ -0,0 +1,21 @@
+module.exports = function (dataset, monthStrings) {
+  let html = `
+    <div class="post-list-results">
+      <div class="post-list-items">
+        ${dataset.map(post => `<div class="post-list-item">
+          <div class="link-title">
+            <a href="${post.data.wordpress_url}">${post.data.title}</a>
+          </div>
+          <div class="date">${dateFormat(post.data.date, monthStrings)}</div>
+        </div>`).join("\n")}
+      </div>
+      <a href="/about-us/announcements" data-type="URL" data-id="/about-us/announcements">View all announcements</a>
+    </div>
+  `;
+  return html;
+}
+
+function dateFormat(dateString, monthStrings) {
+  let d = new Date(dateString);
+  return `${monthStrings[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`
+}
