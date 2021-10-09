0627137b58a4b5f335240e0be09fb3e174557cdb
chachasikes <149436+chachasikes@users.noreply.github.com>
2021-10-09 00:56:21 -0700

    Add more notes and comments

--- .eleventy.js
+++ .eleventy.js
@@ -1,32 +1,35 @@
 const CleanCSS = require("clean-css");
-let postsCollection = require('./src/build/posts.js');
-let lastFewPosts = require('./pages/_data/last-few-posts.js');
-let extractMeta = require('./src/build/extract-meta.js');
+// let postsCollection = require('./src/build/posts.js');
+// let lastFewPosts = require('./pages/_data/last-few-posts.js');
+// let extractMeta = require('./src/build/extract-meta.js');
 
+// @DOCS 11ty reference
 /**
- * @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig 
+ * @param {import("@11ty/eleventy/src/UserConfig")} eleventyConfig
  */
-module.exports = function(eleventyConfig) {
-
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "./src/css/fonts": "fonts" });
   eleventyConfig.addPassthroughCopy({ "./src/rootcopy/*": "/" });
-  
-  // @TODO: Why are we doing this re-write. Let's explain our logic a little for everyone using the code:
-  eleventyConfig.addPassthroughCopy({ "wordpress/media": "wp-content/uploads" });
-  eleventyConfig.addPassthroughCopy({ "dist/index.css.map": "/index.css.map" });
-  
+
+  // @Q: Why are we doing this re-write? Let's explain our logic a little for everyone using the code.
+  eleventyConfig.addPassthroughCopy({
+    "wordpress/media": "wp-content/uploads",
+  });
+  eleventyConfig.addPassthroughCopy({ "dist/index.css.map": "/index.css.map" }); // @Q Does this order matter?
   eleventyConfig.setBrowserSyncConfig({
-    watch:true,
-    notify:true,
- });
- 
-  eleventyConfig.addFilter("cssmin", function(code) {
+    watch: true,
+    notify: true,
+  });
+
+  // Make CSS smaller
+  eleventyConfig.addFilter("cssmin", function (code) {
     return new CleanCSS({}).minify(code).styles;
   });
 
-  eleventyConfig.addFilter("dateFormat", function(dateString) {
+  // @Q How is this used?
+  eleventyConfig.addFilter("dateFormat", function (dateString) {
     let d = new Date(dateString);
-    return `${monthStrings[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`
+    return `${monthStrings[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
   });
 
   return {
@@ -36,6 +39,6 @@ module.exports = function(eleventyConfig) {
     dir: {
       input: "pages",
       output: "docs",
-    }
+    },
   };
-}
+};
