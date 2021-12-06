e891d553ecb7850e3b520571e55c4b49d20e2572
Konstantin Koryaka <konstantin@konstantinsimac.attlocal.net>
2021-12-06 14:56:32 -0800

    replaced pdf-icons and external-icon componetns with link-icon component #443

--- src/js/index-headless.js
+++ src/js/index-headless.js
@@ -6,12 +6,9 @@ import '@cagov/ds-plus';
 import '@cagov/ds-google-translate';
 import '@cagov/ds-dropdown-menu';
 import '@cagov/ds-content-navigation';
-import '@cagov/ds-pdf-icon/src/index.js';
-import '@cagov/ds-external-link-icon/src/index.js';
+import '@cagov/ds-link-icon/src/index.js';
 import '@cagov/ds-back-to-top/src/index.js';
 import '@cagov/ds-statewide-header/src/index.js';
-
-//import '../components/post-list-headless/index.js';
 import '../components/page-alert/index.js';
 
 import './headless/google-analytics.js';
