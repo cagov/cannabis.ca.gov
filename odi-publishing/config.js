c27027fd979ae4e77c7142dc5eff11da5f08f61f
Chach Sikes <chachsikes@ChachSikes.local>
2021-12-28 12:48:02 -0800

    add testing config file and try setting NODE_ENV in yml file

--- odi-publishing/config.js
+++ odi-publishing/config.js
@@ -1,11 +1,14 @@
 const main = require('./odi-publishing.json'); // Default settings.
-
+const test = require('./odi-publishing.test.json'); // Default settings.
 // const staging = require('./odi-publishing.staging.json'); // Default settings.
 const localhost = require('./odi-publishing.localhost.json'); // Default settings.
 
 const getConfig = () => {
     if (process.env.NODE_ENV === "development") {
         return localhost;
+    }
+    if (process.env.NODE_ENV === "test") {
+        return test;
     }
     return main;
 };
