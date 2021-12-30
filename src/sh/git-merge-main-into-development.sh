2710c0f3d511f093387479a1a6a52ec962310283
Chach Sikes <chachsikes@ChachSikes.local>
2021-12-29 16:26:28 -0800

    issue with menu directories, simplifying commands to debug them

--- src/sh/git-merge-main-into-development.sh
+++ src/sh/git-merge-main-into-development.sh
@@ -14,13 +14,13 @@ git merge --no-ff --no-commit main
 #     src/templates/wordpress-media \ 
 #     odi-publishing
 # Also do a checkout to fully prevent any content merges
-git checkout main src/templates/_data \ 
-    src/templates/wordpress/menu \ 
-    src/templates/wordpress/pages \ 
-    src/templates/wordpress/posts \ 
-    src/templates/wordpress/redirects \ 
-    src/templates/wordpress-media \ 
-    src/sh \ 
-    odi-publishing \ 
-    .github/workflows 
+git checkout main src/templates/_data
+git checkout main src/templates/wordpress/menu
+git checkout main src/templates/wordpress/pages
+git checkout main src/templates/wordpress/posts
+git checkout main src/templates/wordpress/redirects
+git checkout main src/templates/wordpress-media
+git checkout main src/sh
+git checkout main odi-publishing
+git checkout main .github/workflows
 #git commit -m "Selective merge: main into development"
--- src/sh/git-merge-test-into-main.sh
+++ src/sh/git-merge-test-into-main.sh
@@ -14,13 +14,13 @@ git merge --no-ff --no-commit test
 #     src/templates/wordpress-media \ 
 #     odi-publishing
 # Also do a checkout to fully prevent any content merges
-git checkout main src/templates/_data \ 
-    src/templates/wordpress/menu \ 
-    src/templates/wordpress/pages \ 
-    src/templates/wordpress/posts \ 
-    src/templates/wordpress/redirects \ 
-    src/templates/wordpress-media \ 
-    src/sh \ 
-    odi-publishing \ 
-    .github/workflows 
+git checkout test src/templates/_data
+git checkout test src/templates/wordpress/menu
+git checkout test src/templates/wordpress/pages
+git checkout test src/templates/wordpress/posts
+git checkout test src/templates/wordpress/redirects
+git checkout test src/templates/wordpress-media
+git checkout test src/sh
+git checkout test odi-publishing
+git checkout test .github/workflows
 #git commit -m "Selective merge: test into main"
