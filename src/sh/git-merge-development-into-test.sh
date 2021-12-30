12b651e63d132034555920d0c06eaf87b9053d30
Chach Sikes <chachsikes@ChachSikes.local>
2021-12-29 16:16:47 -0800

    Sync sh scripts from test branch

---
+++ src/sh/git-merge-development-into-test.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+git fetch
+git checkout development
+git pull
+git checkout test
+git pull
+git merge --no-ff --no-commit development
+git restore --source=HEAD --staged --worktree -- \ 
+    src/templates/_data \ 
+    src/templates/wordpress/menu \ 
+    src/templates/wordpress/pages \ 
+    src/templates/wordpress/posts \ 
+    src/templates/wordpress/redirects \ 
+    src/templates/wordpress-media \ 
+    odi-publishing
+# Also do a checkout to fully prevent any content merges
+git checkout development src/templates/_data \ 
+    src/templates/wordpress/menu \ 
+    src/templates/wordpress/pages \ 
+    src/templates/wordpress/posts \ 
+    src/templates/wordpress/redirects \ 
+    src/templates/wordpress-media \ 
+    odi-publishing
+#git commit -m "Selective merge: development into test"
---
+++ src/sh/git-merge-test-into-main.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+git fetch
+git checkout test
+git pull
+git checkout main
+git pull
+git merge --no-ff --no-commit test
+git restore --source=HEAD --staged --worktree -- \ 
+    src/templates/_data \ 
+    src/templates/wordpress/menu \ 
+    src/templates/wordpress/pages \ 
+    src/templates/wordpress/posts \ 
+    src/templates/wordpress/redirects \ 
+    src/templates/wordpress-media \ 
+    odi-publishing
+# Also do a checkout to fully prevent any content merges
+git checkout main src/templates/_data \ 
+    src/templates/wordpress/menu \ 
+    src/templates/wordpress/pages \ 
+    src/templates/wordpress/posts \ 
+    src/templates/wordpress/redirects \ 
+    src/templates/wordpress-media \ 
+    odi-publishing
+#git commit -m "Selective merge: test into main"
