34af1588c5a6980b5f1fc1425e86f1e8c1e84fe4
Chach Sikes <chachsikes@ChachSikes.local>
2021-12-29 16:24:29 -0800

    issue with menu directories, simplifying commands to debug them

--- src/sh/git-merge-development-into-test.sh
+++ src/sh/git-merge-development-into-test.sh
@@ -14,13 +14,13 @@ git merge --no-ff --no-commit development
 #     src/templates/wordpress-media \ 
 #     odi-publishing
 # Also do a checkout to fully prevent any content merges
-git checkout development src/templates/_data \ 
-    src/templates/wordpress/menu \ 
-    src/templates/wordpress/pages \ 
-    src/templates/wordpress/posts \ 
-    src/templates/wordpress/redirects \ 
-    src/templates/wordpress-media \ 
-    src/sh \ 
-    odi-publishing \ 
-    .github/workflows
+git checkout development src/templates/_data
+git checkout development src/templates/wordpress/menu
+git checkout development src/templates/wordpress/pages
+git checkout development src/templates/wordpress/posts
+git checkout development src/templates/wordpress/redirects
+git checkout development src/templates/wordpress-media
+git checkout development src/sh
+git checkout development odi-publishing
+git checkout development .github/workflows
 #git commit -m "Selective merge: development into test"
