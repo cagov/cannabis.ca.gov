62f37cf7b514e4bee5b78e868da0db2ba063a357
Chach Sikes <chachsikes@ChachSikes.local>
2022-01-10 07:26:08 -0800

    Sync merge scripts between development and test branches

--- src/sh/git-merge-test-into-main.sh
+++ src/sh/git-merge-test-into-main.sh
@@ -5,22 +5,20 @@ git pull
 git checkout main
 git pull
 git merge --no-ff --no-commit test
-# git restore --source=HEAD --staged --worktree -- \ 
-#     src/templates/_data \ 
-#     src/templates/wordpress/menu \ 
-#     src/templates/wordpress/pages \ 
-#     src/templates/wordpress/posts \ 
-#     src/templates/wordpress/redirects \ 
-#     src/templates/wordpress-media \ 
-#     odi-publishing
+git restore --source=HEAD --staged --worktree -- \ 
+    src/templates/_data \ 
+    src/templates/wordpress/menu \ 
+    src/templates/wordpress/pages \ 
+    src/templates/wordpress/posts \ 
+    src/templates/wordpress/redirects \ 
+    src/templates/wordpress-media \ 
+    odi-publishing
 # Also do a checkout to fully prevent any content merges
-git checkout main src/templates/_data
-git checkout main src/templates/wordpress/menu
-git checkout main src/templates/wordpress/pages
-git checkout main src/templates/wordpress/posts
-git checkout main src/templates/wordpress/redirects
-git checkout main src/templates/wordpress-media
-git checkout main src/sh
-git checkout main odi-publishing
-git checkout main .github/workflows
+git checkout main src/templates/_data \ 
+    src/templates/wordpress/menu \ 
+    src/templates/wordpress/pages \ 
+    src/templates/wordpress/posts \ 
+    src/templates/wordpress/redirects \ 
+    src/templates/wordpress-media \ 
+    odi-publishing
 #git commit -m "Selective merge: test into main"
