674221c194e8687f0ece24d9f315cb8411d7ceda
Chach Sikes <chachsikes@ChachSikes.local>
2022-03-07 22:18:49 -0800

    make a simple local server to build interaction in javascript only with local data.

---
+++ src/js/charts/cannabis-legal-access/src/index.js
@@ -0,0 +1,11 @@
+console.log("Cannabis Legal Activity");
+
+var width = 800;
+var height = 930;
+
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
