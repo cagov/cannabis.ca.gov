60616146e8615c5abf41f0521e63038624b1bf13
Chach Sikes <chachsikes@ChachSikes.local>
2022-03-08 15:36:02 -0800

    get a hotloader to refresh the page

--- src/js/charts/cannabis-legal-access/src/index.js
+++ src/js/charts/cannabis-legal-access/src/index.js
@@ -1,15 +1,21 @@
-import './index.css';
 import dataLayers from "./data-layers.js";
 
-console.log("Cannabis Legal Activity 4");
-console.log(dataLayers);
+console.log("Cannabis Legal Activity 12");
+console.log("test" , dataLayers);
 
 var width = 800;
 var height = 930;
 
-var svg = d3
-  .select("body")
-  .append("svg")
-  .attr("width", width)
-  .attr("height", height);
+dataLayers.map((layer) => {
+  console.log(layer.src);
+  // src, layerName, width, height
+
+  // d3.xml(layer.src)
+  // .then(data => {
+  //   d3.select("#county-map .svg-layer-container").node().append(data.documentElement)
+  //     .attr("width", layer.width || width)
+  //     .attr("height", layer.height || height)
+  // });
+});
+
 
--- src/js/charts/cannabis-legal-access/webpack.config.js
+++ src/js/charts/cannabis-legal-access/webpack.config.js
@@ -1,73 +1,109 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
-var webpack = require('webpack');
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-const isProduction = process.env.NODE_ENV == "production";
-
-console.log("Is production: ", isProduction);
-
-const stylesHandler = isProduction
-  ? MiniCssExtractPlugin.loader
-  : "style-loader";
-
-const config = {
-  entry: [
-    "webpack/hot/dev-server",
-    "webpack-hot-middleware/client",
-    "./src/index.js",
-  ],
+module.exports = {
+  entry: "./src/index.js",
   output: {
-    filename: 'index.js',
     path: path.resolve(__dirname, "dist"),
-    publicPath: '/dist'
+    filename: "bundle.js",
   },
-  devServer: {
-    open: true,
-    host: "localhost",
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
-
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
+  // devServer: {
+  //   stats: {
+  //     children: false,
+  //     maxModules: 0,
+  //   },
+  //   port: 3001,
+  // },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/i,
-        loader: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
       },
       {
-        test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [stylesHandler, "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
 };
 
-module.exports = () => {
-  if (isProduction) {
-    config.mode = "production";
+// // Generated using webpack-cli https://github.com/webpack/webpack-cli
+// var webpack = require('webpack');
+// const path = require("path");
+// const HtmlWebpackPlugin = require("html-webpack-plugin");
+// const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-    config.plugins.push(new MiniCssExtractPlugin());
-  } else {
-    config.mode = "development";
-  }
-  return config;
-};
+// const isProduction = process.env.NODE_ENV == "production";
+
+// const stylesHandler = isProduction
+//   ? MiniCssExtractPlugin.loader
+//   : "style-loader";
+
+// const config = {
+//   entry: [
+//     "webpack/hot/dev-server",
+//     "webpack-hot-middleware/client",
+//     "./src/index.js",
+//   ],
+//   output: {
+//     filename: 'index.js',
+//     path: path.resolve(__dirname, "dist"),
+//     publicPath: '/dist'
+//   },
+//   devServer: {
+//     open: true,
+//     host: "localhost",
+//   },
+//   plugins: [
+//     new webpack.HotModuleReplacementPlugin(),
+//     new HtmlWebpackPlugin({
+//       template: "index.html",
+//     }),
+
+//     // Add your plugins here
+//     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/i,
+//         loader: "babel-loader",
+//       },
+//       {
+//         test: /\.css$/i,
+//         use: [stylesHandler, "css-loader"],
+//       },
+//       {
+//         test: /\.s[ac]ss$/i,
+//         use: [stylesHandler, "css-loader", "sass-loader"],
+//       },
+//       {
+//         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+//         type: "asset",
+//       },
+
+//       // Add your rules for custom modules here
+//       // Learn more about loaders from https://webpack.js.org/loaders/
+//     ],
+//   },
+// };
+
+// module.exports = () => {
+//   if (isProduction) {
+//     config.mode = "production";
+
+//     config.plugins.push(new MiniCssExtractPlugin());
+//   } else {
+//     config.mode = "development";
+//   }
+//   return config;
+// };
