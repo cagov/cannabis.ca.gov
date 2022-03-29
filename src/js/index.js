8dea04b6ca01032690990259aa4bb1bef9d13d3b
Chach Sikes <chachsikes@ChachSikes.local>
2022-03-29 11:42:58 -0700

    Updating design system code

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,40 @@
 // Main build file for the JavaScript static site generation bundle.
+// Design system components
 import '@cagov/ds-accordion';
+import '@cagov/ds-agency-footer';
 import '@cagov/ds-back-to-top';
-import '@cagov/ds-content-navigation';
+import '@cagov/ds-branding';
+import '@cagov/ds-button-grid';
+import '@cagov/ds-page-navigation';
 import '@cagov/ds-dropdown-menu';
-import '@cagov/ds-feedback/dist/index.js';
+import '@cagov/ds-feedback';
 import '@cagov/ds-google-translate';
-import '@cagov/ds-link-icon/src/index.js';
-import '@cagov/ds-minus';
+import '@cagov/ds-link-grid';
+import '@cagov/ds-link-icon';
+import '@cagov/ds-page-alert';
 import '@cagov/ds-pagination';
-import { controlPages } from '../components/pagination/control.js'
-import '@cagov/ds-plus';
-import '@cagov/ds-statewide-header/src/index.js';
-import '@cagov/ds-page-alert/src/index.js';
-// Data viz component
-import './charts/cannabis-legal-access/build/bundle.js';
+import '@cagov/ds-regulatory-outline';
+import '@cagov/ds-site-footer';
+import '@cagov/ds-site-header';
+import '@cagov/ds-site-navigation';
+import '@cagov/ds-skip-to-content';
+import '@cagov/ds-statewide-header';
+import '@cagov/ds-statewide-footer';
+import '@cagov/ds-step-list';
+import '@cagov/ds-table';
 
+// Site modifications
+// import { controlPages } from '../components/pagination/control.js'
+import { controlPages } from '@cagov/ds-pagination';
 import './google-analytics.js';
 import setupAnalytics from './setup-analytics.js';
+
+
+// Custom components
+// Data viz: consumers/where-cannabis-business-is-legal-in-california
+import './charts/cannabis-legal-access/src/index.js';
+// import './charts/cannabis-legal-access/build/bundle.js';
+
 
 window.onload = (event) => {
   setupAnalytics();
