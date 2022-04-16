91c84ec3f8b5c8049e2ececf5c0c0df70ad11955
Zakiya <zakiya.khabir@digital.ca.gov>
2022-04-15 18:46:47 -0700

    #484 - Rollup doesn't like the way the w3 js files are writtens so stick them all in one file

--- src/js/charts/cannabis-local-ordinances/src/js/places/rollup.config.js
+++ src/js/charts/cannabis-local-ordinances/src/js/places/rollup.config.js
@@ -2,12 +2,7 @@ import css from "rollup-plugin-import-css";
 import multi from "@rollup/plugin-multi-entry";
 
 export default {
-  input: [
-    "src/index.js",
-    "src/w3-combobox/combobox-1.0-list.js",
-    "src/w3-combobox/listbox.js",
-    "src/w3-combobox/listboxOption.js",
-  ],
+  input: ["src/index.js", "src/w3-combobox/combobox-1.0-list.js"],
   output: { file: "dist/index.js", format: "esm" },
   plugins: [css(), multi()],
 };
