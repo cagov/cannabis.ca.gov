d3ef9bdbbac3774f8522b7bb0d97d899a87b3944
Chach Sikes <chachsikes@ChachSikes.local>
2022-04-17 00:02:41 -0700

    update tooltips

--- src/js/charts/cannabis-local-ordinances/src/js/county-map/tooltipPlace.js
+++ src/js/charts/cannabis-local-ordinances/src/js/county-map/tooltipPlace.js
@@ -49,20 +49,12 @@ function placeStatusTooltipMessage(data, props, options) {
   let { all, city, county, prohibited, allowed, prohibitedLegend, allowedLegend, detailsCTA } =
     getToolTipMessages(data, name, props, "City");
 
-  //   let isAllowed = null;
-
-  // if (currentCounty["Are all CCA activites prohibited?"] === "Yes") {
-  //   isAllowed = false;
-  // } else {
-  //   isAllowed = true;
-  // }
 
 
-  // Choose label based on toggle
-  // let label = all;
 
   data.tooltipData = getPlaceTooltipData(data, props);
   let label = "";
+
   label = insertValueIntoSpanTag(label, mode, "data-status");
 
   let icon = "";
@@ -159,13 +151,14 @@ function getPlaceTooltipData(data, props) {
 
 function getToolTipMessages(data, name, props, jurisdiction) {
   let { messages, activities } = data;
-  // console.log("jur", jurisdiction);
+  console.log("jur", jurisdiction);
   let mode = activities;
   if (mode === "Any cannabis business" && jurisdiction === "County") {
-    // console.log("county");
+    console.log("county");
     return messages["TooltipCountyAllActivities"];
+    
   } else if (mode === "Any cannabis business" && jurisdiction === "City") {
-    // console.log("city");
+    console.log("city");
     return messages["TooltipPlaceAllActivities"];
   } else {
     if (jurisdiction === "County") {
