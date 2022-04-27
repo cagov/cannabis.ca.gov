84ff85445135ca3aacadad0473f0c68302d168a1
Zakiya <zakiya.khabir@digital.ca.gov>
2022-04-27 16:20:31 -0700

    484 - combobox - We don't seem to need updatePlacesFilter because the combobox does it for us.

--- src/js/charts/cannabis-local-ordinances/src/js/county-map/updateHistory.js
+++ src/js/charts/cannabis-local-ordinances/src/js/county-map/updateHistory.js
@@ -1,4 +1,3 @@
-import updatePlacesListFilter from "./updatePlacesListFilter.js";
 /**
  * Set path for interaction state.
  * @param {*} props
@@ -13,15 +12,7 @@ const updateHistory = (props) => {
   // window.location.hash = path; // TEMPORARILY DISABLED, Future feature.
 };
 
-const updateMapJurisdictionDisplayFromHash = (hash, data) => {
-  // console.log("update hash", hash, data);
-  // if (hash !== undefined && hash.length > 0) {
-  //   let paramKeys = getParamKeys(hash, data);
-  //   console.log("paramKeys", paramKeys);
-  //   setDataFromHash(paramKeys, data);
-  //   updateDisplaysFromInteraction(data);
-  // }
-};
+const updateMapJurisdictionDisplayFromHash = (hash, data) => {};
 
 const getParamKeys = (hash, data) => {
   let paramKeys = {};
@@ -39,7 +30,6 @@ const getParamKeys = (hash, data) => {
       });
     }
   }
-  // console.log("paramKeys", paramKeys);
   return paramKeys;
 };
 
@@ -79,9 +69,6 @@ const setDataFromHash = (paramKeys, data) => {
 /** Set filters after interaction */
 const updateDisplaysFromInteraction = (data) => {
   updateActivityFilter(data);
-  updatePlacesListFilter(data);
-  // @todo Deprecate updatePlacesFilter() once previous function is testedand verified.
-  // updatePlacesFilter(data);
 };
 
 const updateActivityFilter = (data) => {
@@ -97,70 +84,6 @@ const updateActivityFilter = (data) => {
   if (updateOptionActivity !== null) {
     setActivityFilterEl.selected = false;
     updateOptionActivity.selected = true;
-  }
-};
-
-const updatePlacesFilter = (data) => {
-  // Update places filter settings
-  // Clear existing options
-  var setPlaceFilterEl = document.querySelector(
-    '.filter[data-filter-type="places"]'
-  );
-  var setPlaceFilterOptionsEl = document.querySelector(
-    '.filter[data-filter-type="places"] select option'
-  );
-
-  if (setPlaceFilterOptionsEl !== null) {
-    let value = setPlaceFilterEl.value;
-    if (data.jurisdiction === "County") {
-      if (updateOptionCountyEl !== null) {
-        var updateOptionCountyEl = document.querySelector(
-          `.filter[data-filter-type="places"] select option[value="${data.selectedCounty}"]`
-        );
-        let jurisdiction =
-          updateOptionCountyEl.getAttribute("data-jurisdiction");
-        if (jurisdiction === "County" && value !== data.selectedCounty) {
-          if (updateOptionCountyEl !== null) {
-            setPlaceFilterOptionsEl.selected = false; // Unset anything selected.
-            updateOptionCountyEl.selected = true;
-            // const e = new CustomEvent("change", {
-            //   detail: { el: updateOptionCountyEl, hash: true },
-            // });
-
-            // setPlaceFilterEl.dispatchEvent(e);
-          }
-        }
-      }
-    } else if (data.jurisdiction === "Place") {
-      var updateOptionPlaceEl = document.querySelector(
-        `.filter[data-filter-type="places"] select option[data-geoid="${data.geoid}"]`
-      );
-
-      if (updateOptionPlaceEl !== null) {
-        let jurisdiction =
-          updateOptionPlaceEl.getAttribute("data-jurisdiction");
-        let optionGeoid = updateOptionPlaceEl.getAttribute("data-geoid");
-
-        if (
-          updateOptionPlaceEl !== null &&
-          data.geoid !== null &&
-          optionGeoid !== data.geoid
-        ) {
-          setPlaceFilterEl.selected = false; // Unset anything selected.
-          updateOptionPlaceEl.selected = true;
-        }
-      }
-    } else if (data.jurisdiction === "Statewide") {
-      // @TODO confirm
-      var updateOptionStatewideEl = document.querySelector(
-        `.filter[data-filter-type="places"] select option[value=""]`
-      );
-
-      if (updateOptionStatewideEl !== null) {
-        setPlaceFilterEl.selected = false;
-        updateOptionStatewideEl.selected = true;
-      }
-    }
   }
 };
 
