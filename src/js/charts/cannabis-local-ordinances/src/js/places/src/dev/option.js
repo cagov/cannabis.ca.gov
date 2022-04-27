aab29e52c7b8e5b60eddc821faca6efa778269ca
Zakiya <zakiya.khabir@digital.ca.gov>
2022-04-27 08:13:52 -0700

    484 - combobox - Use data-value instead of value as an li attribute.

--- src/js/charts/cannabis-local-ordinances/src/js/places/src/dev/option.js
+++ src/js/charts/cannabis-local-ordinances/src/js/places/src/dev/option.js
@@ -5,7 +5,7 @@ export class Option {
     this.geoAttr =
       this.place.geoid != null ? `place-geoid="${this.place.geoid}"` : "";
     this.jurisAttr = 'data-jurisdiction="' + this.place.dataJurisdiction + '"';
-    this.valAttr = 'value="' + this.place.value + '"';
+    this.valAttr = 'data-value="' + this.place.value + '"';
   }
 
   html() {
