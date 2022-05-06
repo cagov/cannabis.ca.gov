1b5a4920f8b2743beabd3cab88382c05e94ab246
Zakiya <zakiya.khabir@digital.ca.gov>
2022-05-06 08:52:32 -0700

    484 - combobox - Improve clear button appearance.

--- src/js/charts/cannabis-local-ordinances/src/js/places/src/clear.js
+++ src/js/charts/cannabis-local-ordinances/src/js/places/src/clear.js
@@ -1,5 +1,6 @@
 const button = document.querySelector(".combobox-places--button");
 const input = document.querySelector(".cb_edit");
-button.addEventListener("click", () => {
+button.addEventListener("click", (e) => {
+  e.preventDefault();
   input.value = "";
 });
