71e5a0953c2387fcdef8ed21d12b94c73e293227
Zakiya <zakiya.khabir@digital.ca.gov>
2022-05-06 09:02:53 -0700

    484 - combobox - Move clear js to main file.

--- src/js/charts/cannabis-local-ordinances/src/js/places/src/clear.js
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector(".combobox-places--button");
-const input = document.querySelector(".cb_edit");
-button.addEventListener("click", (e) => {
-  e.preventDefault();
-  input.value = "";
-});
