7664259886aa29262298e48f11ce22fefbd4db85
Chach Sikes <chach.sikes@innovation.ca.gov>
2022-09-12 12:40:34 -0700

    Turn off most CSS; Add some newer design token colors

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,23 @@
 // Main build file for the JavaScript static site generation bundle.
+import "@cagov/ds-site-navigation";
+import "@cagov/ds-page-feedback";
+import "@cagov/ds-page-navigation";
+import "@cagov/ds-link-grid";
+
 // Design system components
 import '@cagov/ds-accordion';
 import '@cagov/ds-back-to-top';
-import '@cagov/ds-page-navigation';
-import '@cagov/ds-dropdown-menu';
-import '@cagov/ds-page-feedback/src/index.js';
-import '../components/ds-page-feedback/src/index.js'; // For drafting microcopy update.
-import '@cagov/ds-google-translate';
-import '@cagov/ds-link-icon/src/index.js';
-import '@cagov/ds-page-alert';
-import '@cagov/ds-statewide-header/src/index.js';
+
+// import '@cagov/ds-dropdown-menu';
+// import '@cagov/ds-page-feedback/src/index.js';
+// import '../components/ds-page-feedback/src/index.js'; // For drafting microcopy update.
+// import '@cagov/ds-google-translate';
+// import '@cagov/ds-link-icon/src/index.js';
+// import '@cagov/ds-page-alert';
+// import '@cagov/ds-statewide-header/src/index.js';
 
 // Site modifications
 import { controlPages } from '../js/pagination.js';
-
 
 import './google-analytics.js';
 import setupAnalytics from './setup-analytics.js';
