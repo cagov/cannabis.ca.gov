cc7875c905d903c047919e34e2445bc8121356a4
Chach Sikes <chach.sikes@innovation.ca.gov>
2022-09-20 19:13:30 -0700

    Hello work LinkChecker tests - http-server port needed to be set to 8080 to match the port preferences in webServer settings.

--- test/playwright-dev.config.js
+++ test/playwright-dev.config.js
@@ -20,26 +20,3 @@ const config = {
 };
 
 export default config;
-
-
-// const config = {
-//   retries: 0,
-//   timeout: 30000,
-//   use: {
-//     headless: true,
-//     ignoreHTTPSErrors: false,
-//     trace: "on",
-//     video: "on",
-//     baseURL: "http://localhost:8080",
-//     reporter: "line",
-//   },
-//   /* Run your local dev server before starting the tests */
-//   webServer: {
-//     command: "npm run test:serve",
-//     port: 8080,
-//     timeout: 30 * 1000,
-//     reuseExistingServer: false,
-//   },
-// };
-
-// export default config;
