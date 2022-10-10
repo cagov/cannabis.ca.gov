2d5cfae61dabe0088e114a9c6e44a65fcea6c9bb
Chach Sikes <chach.sikes@innovation.ca.gov>
2022-10-10 14:19:30 -0700

    Reenable redirects script

--- src/deploy/redirects/assign.js
+++ src/deploy/redirects/assign.js
@@ -2,26 +2,26 @@ const fs = require("fs");
 const AWS = require("aws-sdk");
 
 const s3 = new AWS.S3();
-const config = require("../../../config");
+const config = require("../../../config/index.js");
 
 const redirectFile = JSON.parse(
   fs.readFileSync(`${config.staticContentPaths.redirects}/redirects.json`)
 );
 
-// redirectFile.redirects.forEach((redirect) => {
-//   console.log("Uploading ", redirect);
-//   s3.putObject(
-//     {
-//       Body: "",
-//       Bucket: config.build.host_name,
-//       Key:
-//         redirect.url.indexOf("/") === 0
-//           ? redirect.url.substring(1)
-//           : redirect.url,
-//       WebsiteRedirectLocation: redirect.action_data.url,
-//     },
-//     (err, data) => {
-//       if (err) return console.log(err);
-//     }
-//   );
-// });
+redirectFile.redirects.forEach((redirect) => {
+  console.log("Uploading ", redirect);
+  s3.putObject(
+    {
+      Body: "",
+      Bucket: config.build.host_name,
+      Key:
+        redirect.url.indexOf("/") === 0
+          ? redirect.url.substring(1)
+          : redirect.url,
+      WebsiteRedirectLocation: redirect.action_data.url,
+    },
+    (err, data) => {
+      if (err) return console.log(err);
+    }
+  );
+});
