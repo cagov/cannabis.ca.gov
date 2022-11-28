15deb0e0f36a6dfa04e6f5e2d1e4555d784cedd2
Chach Sikes <chach.sikes@innovation.ca.gov>
2022-11-28 14:58:57 -0800

    Add redirects tests; add notifications test yml code in staging branch (will only test production redirects RN - checking availability of redirects.json in Github Action)

--- test/redirects.spec.mjs
+++ test/redirects.spec.mjs
@@ -1,22 +1,30 @@
 import { test, expect } from "@playwright/test";
 import { findAll } from "domutils";
+import { readFile } from "fs/promises";
+try {
+  const redirects = JSON.parse(
+    await readFile("./pages/_content/redirects/redirects.json")
+  );
 
-import redirects from "./redirects.js";
+  const redirectData = redirects.redirects;
 
-class Entry {
-  constructor(slug) {
-    this.page = `${slug}/`;
+  class Entry {
+    constructor(slug) {
+      this.page = `${slug}/`;
+    }
   }
-}
-const redirectData = redirects.data.redirects;
-test.describe.serial("redirectPathChecks", () => {
- redirectData.map((redirect) => {    
-    let requestPath = "https://cannabis.ca.gov" + redirect.url;
-    let requestTarget = "https://cannabis.ca.gov" + redirect.action_data.url;
-    test("Check redirect for: " + requestPath, async ({ page }) => {
-      const entry = new Entry(requestPath);
-      await page.goto(requestPath);
-      expect(page.url()).toBe(requestTarget);
+
+  test.describe.serial("redirectPathChecks", () => {
+   redirectData.map((redirect) => {
+      let requestPath = "https://cannabis.ca.gov" + redirect.url;
+      let requestTarget = "https://cannabis.ca.gov" + redirect.action_data.url;
+      test("Check redirect for: " + requestPath + " " + redirect.id, async ({ page }) => {
+        const entry = new Entry(requestPath);
+        await page.goto(requestPath);
+        expect(page.url()).toBe(requestTarget);
+      });
     });
   });
-});
+} catch (error) {
+  console.error(error);
+}
